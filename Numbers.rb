e1ba2a18b9e093a5abca49d0adb42dc28710ff85
Rob1nHood1e <vladyugay64@gmail.com>
2021-03-26 02:30:11 +0300

    6 задание(дополнение)

--- Numbers.rb
+++ Numbers.rb
@@ -11,6 +11,15 @@ def digits_sum(x,q=10)
   res
 end
 
+def digits_mult(x,q=10)
+  res = 1
+  while x > 0
+    res *= last_digit(x,q)
+    x /= q
+  end
+  res
+end
+
 def max_digit(x,q=10)
   max = last_digit(x,q)
   x /= q
@@ -23,6 +32,22 @@ def max_digit(x,q=10)
   max
 end
 
+def min_digit(x,q=10)
+  min = last_digit(x,q)
+  x /= q
+  while x > 0
+    if min > last_digit(x,q)
+      min = last_digit(x,q)
+    end
+    x /= q
+  end
+  min
+end
+
+
+
 num = ARGV[0].to_i
 puts digits_sum(num)
 puts max_digit(num)
+puts min_digit(num)
+puts digits_mult(num)
